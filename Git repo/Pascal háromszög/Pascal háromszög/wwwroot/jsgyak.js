69f2672df0976b149d6365a7f0b35763b988f504
corvinus-d86y6c <79813061+corvinus-d86y6c@users.noreply.github.com>
2022-11-23 13:04:31 +0100

    Add files via upload

---
+++ Git repo/Pascal háromszög/Pascal háromszög/wwwroot/jsgyak.js
@@ -0,0 +1,15 @@
+﻿var faktoriális = function (n) {
+    let er = 1;
+    for (let i = 2; i <= n; i++) {
+        er = er * i;
+    }
+    return er;
+}
+
+var faktoriálisR = (n) => {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * faktoriálisR(n - 1)
+    }
+}
